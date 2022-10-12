d1f31b7fe2a6f40ba7b5dee26920af0e8941ab12
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-10-12 14:19:59 +0300

    added serialization to commodity

--- app/controllers/bidders_controller.rb
+++ app/controllers/bidders_controller.rb
@@ -2,5 +2,6 @@ class BiddersController < ApplicationController
     def index
         bidders = Bidder.all
         render json: bidders,include: :comment
+        
     end
 end
---
+++ app/serializers/commodity_serializer.rb
@@ -0,0 +1,3 @@
+class CommoditySerializer < ActiveModel::Serializer
+  attributes :name,:category,:bid_price,:image_url,:description
+end
