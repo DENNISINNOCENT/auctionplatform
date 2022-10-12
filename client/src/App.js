9c20e7cee8156847e614f64f87fedad02378debd
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-10-12 23:25:36 +0300

    added the signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Login from './components/Login';
 import './App.css';
+import SignUp from './components/Signup';
 
 function App() {
   return (
     <div className="App">
       <Login/>
+      <SignUp/>
     </div>
   );
 }
