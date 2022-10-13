4cde0918f686f25bda0a9da20083f1263cf97728
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-10-13 05:45:15 +0300

    added navigation to the app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,47 @@
-import Login from './components/Login';
-import './App.css';
-import SignUp from './components/Signup';
-import Home from './components/Home';
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./components/Signup";
+import Login from "./components/Login";
+import NavBar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <Login/>
-      <SignUp/>
-      <Home/>
-    </div>
+    <>
+      <NavBar user={user} setUser={setUser} />
+      <main>
+        {user ? (
+          <Routes>
+            <Route path="/">
+              <Home user={user}/>
+            </Route>
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/signup">
+              <SignUp setUser={setUser} />
+            </Route>
+            <Route path="/login">
+              <Login setUser={setUser} />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Routes>
+        )}
+      </main>
+    </>
   );
 }
 
